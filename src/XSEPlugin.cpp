8a6e1175d6914c4e4509f880afe0d882109073c5
doodlum <15017472+doodlum@users.noreply.github.com>
2025-12-02 17:50:06 +0000

    chore: more stuff

--- src/XSEPlugin.cpp
+++ src/XSEPlugin.cpp
@@ -1,6 +1,12 @@
 
-#include "DX11Hooks.h"
-#include "Upscaling.h"
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <fstream>
+#include <wrl/client.h>
+
+#pragma comment(lib, "dxguid.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
 
 void InitializeLog()
 {
@@ -55,15 +61,115 @@ extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Query(const F4SE::QueryInterface*, 
 }
 #endif
 
-void MessageHandler(F4SE::MessagingInterface::Message* message)
+bool initialized = false;
+
+void AddDebugInformation()
 {
-	switch (message->type) {
-	case F4SE::MessagingInterface::kPostPostLoad:
-		{
-			Upscaling::GetSingleton()->PostPostLoad();
-			break;
+	auto rendererData = RE::BSGraphics::RendererData::GetSingleton();
+
+	for (uint32_t i = 0; i < 101; i++) {
+		if (auto texture = rendererData->renderTargets[i].texture) {
+			auto name = std::format("RT {}", i);
+			texture->SetPrivateData(WKPDID_D3DDebugObjectName, static_cast<UINT>(name.size()), name.data());
+		}
+		if (auto texture = rendererData->renderTargets[i].copyTexture) {
+			auto name = std::format("COPY RT {}", i);
+			texture->SetPrivateData(WKPDID_D3DDebugObjectName, static_cast<UINT>(name.size()), name.data());
+		}
+		if (auto srView = rendererData->renderTargets[i].srView) {
+			auto name = std::format("SRV {}", i);
+			srView->SetPrivateData(WKPDID_D3DDebugObjectName, static_cast<UINT>(name.size()), name.data());
+		}
+		if (auto copySRView = rendererData->renderTargets[i].copySRView) {
+			auto name = std::format("COPY SRV {}", i);
+			copySRView->SetPrivateData(WKPDID_D3DDebugObjectName, static_cast<UINT>(name.size()), name.data());
+		}
+		if (auto rtView = rendererData->renderTargets[i].rtView) {
+			auto name = std::format("RTV {}", i);
+			rtView->SetPrivateData(WKPDID_D3DDebugObjectName, static_cast<UINT>(name.size()), name.data());
+		}
+		if (auto uaView = rendererData->renderTargets[i].uaView) {
+			auto name = std::format("UAV {}", i);
+			uaView->SetPrivateData(WKPDID_D3DDebugObjectName, static_cast<UINT>(name.size()), name.data());
 		}
 	}
+
+	for (uint32_t i = 0; i < 13; i++) {
+		if (auto texture = rendererData->depthStencilTargets[i].texture) {
+			auto name = std::format("DEPTH RT {}", i);
+			texture->SetPrivateData(WKPDID_D3DDebugObjectName, static_cast<UINT>(name.size()), name.data());
+		}
+		if (auto srViewDepth = rendererData->depthStencilTargets[i].srViewDepth) {
+			auto name = std::format("DS VIEW {}", i);
+			srViewDepth->SetPrivateData(WKPDID_D3DDebugObjectName, static_cast<UINT>(name.size()), name.data());
+		}
+	}
+}
+
+void Initialize()
+{
+	if (initialized)
+		return;
+
+	initialized = true;
+
+	AddDebugInformation();
+}
+
+struct BSGraphics_State_UpdateTemporalData
+{
+	static void thunk(RE::BSGraphics::State* This)
+	{
+		func(This);
+	}
+	static inline REL::Relocation<decltype(thunk)> func;
+};
+
+struct ImageSpaceEffectTemporalAA_IsActive
+{
+	static bool thunk(struct ImageSpaceEffectTemporalAA*)
+	{
+		return false;
+	}
+	static inline REL::Relocation<decltype(thunk)> func;
+};
+
+struct ImageSpaceEffectUpsampleDynamicResolution_IsActive
+{
+	static bool thunk(struct ImageSpaceEffectUpsampleDynamicResolution*)
+	{
+		return true;
+	}
+	static inline REL::Relocation<decltype(thunk)> func;
+};
+
+struct BSGraphics__RenderTargetManager__UpdateDynamicResolution
+{
+	static void thunk(RE::BSGraphics::RenderTargetManager* This,
+		float*,
+		float*,
+		float*,
+		float*)
+	{
+		This->dynamicWidthRatio = 0.5;
+		This->dynamicHeightRatio = 0.5;
+
+		This->isDynamicResolutionCurrentlyActivated = true;
+
+		This->SetEnableDynamicResolution(true);
+	}
+	static inline REL::Relocation<decltype(thunk)> func;
+};
+
+static void InstallHooks()
+{
+	stl::detour_thunk<BSGraphics_State_UpdateTemporalData>(REL::ID(376068)); // 141D16C00
+	
+	stl::write_vfunc<0x8, ImageSpaceEffectTemporalAA_IsActive>(RE::VTABLE::ImageSpaceEffectTemporalAA[0]);
+
+	stl::write_vfunc<0x8, ImageSpaceEffectUpsampleDynamicResolution_IsActive>(RE::VTABLE::ImageSpaceEffectUpsampleDynamicResolution[0]);
+	
+	stl::detour_thunk<BSGraphics__RenderTargetManager__UpdateDynamicResolution>(REL::ID(1115215)); // 141D31B90
 }
 
 extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Load(const F4SE::LoadInterface* a_f4se)
@@ -79,14 +185,7 @@ extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Load(const F4SE::LoadInterface* a_f
 #endif
 
 	InitializeLog();
-
-	DX11Hooks::Install();
-	Upscaling::InstallHooks();
-
-	Upscaling::GetSingleton()->LoadSettings();
-
-	auto messaging = F4SE::GetMessagingInterface();
-	messaging->RegisterListener(MessageHandler);
+	InstallHooks();
 
 	return true;
 }
